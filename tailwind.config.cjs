d25c1e057d8ba73617ff0df608efb68541e6f952
hernany <hernanytec777@gmail.com>
2022-11-02 14:50:56 -0300

    style(tailwind): configuring design tokens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: 12,
+      md: 14,
+      lg: 16,
+    },
+    colors: {
+      gray: {
+        100: "#F2F2F2",
+        200: "#D9D9D9",
+        300: "#808080",
+        400: "#333333",
+        500: "#262626",
+        600: "#1A1A1A",
+        700: "#0D0D0D",
+      },
+      blue: "#4EA8DE",
+      "blue-dark": "#1E6F9F",
+      purple: "#8284FA",
+      "purple-dark": "#5E60CE",
+      danger: "#E25858",
+    },
+    extend: {
+      fontFamily: {
+        sans: "Inter, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
